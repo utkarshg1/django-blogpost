4650b2224ef7d63357a13d99fa70ad5aafb83e67
utkarshg1 <gaikwadujg@gmail.com>
2024-09-24 00:41:49 +0530

    updated blogs/views.py

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -74,7 +74,7 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
+    sorted_posts = sorted(all_posts, key=get_date, reverse=True)
     latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {
         "posts": latest_posts
