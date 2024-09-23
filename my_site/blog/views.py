31900aa8d784842e7432b43fa9f610b6f0c5d380
utkarshg1 <gaikwadujg@gmail.com>
2024-09-23 20:10:09 +0530

    added templates urls and views

---
+++ my_site/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def starting_page(request):
+    return render(request, "blog/index.html")
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request):
+    pass
