618b94a79c80ce9d67c02e0a21a2c3599d76ed50
utkarshg1 <gaikwadujg@gmail.com>
2024-09-26 01:26:20 +0530

    file upload completed

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+MEDIA_ROOT = BASE_DIR / "uploads"
+
+MEDIA_URL = "/files/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
